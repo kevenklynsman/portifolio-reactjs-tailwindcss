0db234f766109ee8b9ac604334355fa977cfb7b6
keven klynsman <kevenklynsman2003@gmail.com>
2023-12-01 12:32:44 -0300

    feat: adding alert message

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,6 +4,8 @@ import { InformationContainer } from "./InformationContainer";
 import Avatar from "../img/foto_de_perfil.png";
 
 export const SideBar = () => {
+  const alertClick = () => alert("Solicite o Currículo pelo email a cima");
+
   return (
     <>
       <aside className="bg-blue-600 md:flex-1 m-auto md:m-0 md:p-10 mb-5 p-5 rounded-xl flex items-center flex-col text-center relative ">
@@ -18,7 +20,8 @@ export const SideBar = () => {
         <SocialNetworks />
         <InformationContainer />
         <a
-          href=""
+          href="#"
+          onClick={alertClick}
           className="py-3 px-4 text-gray-300 text-none rounded-md bg-gradient-to-r from-cyan-500 to-blue-500"
         >
           Download do currículo
